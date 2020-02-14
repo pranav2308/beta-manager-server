e7119a47804136b561df5b56fc949bf482083e28
pranav2308 <pranav08@terpmail.umd.edu>
2020-02-14 17:29:11 -0500

    Switched to dynamic port and removed nodemon from npm start for deployment

--- server.js
+++ server.js
@@ -13,27 +13,20 @@ const spawn = require("child_process").spawn;
 
 var knex = require('knex')
 
-// const database = knex({
-//   client: 'pg',
-//   connection: {
-//     connectionString: process.env.DATABASE_URL,
-//   	ssl: true
-//   }
-// });
-
 const database = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'pranav2308',
-    database : 'beta_manager'
+    connectionString: process.env.DATABASE_URL,
+  	ssl: true
   }
 });
 
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+	res.send('Successful Deployment!');
+});
 app.post('/register', registerUser(bcrypt, database));
 
 app.post('/login', loginUser(bcrypt, database));
@@ -42,11 +35,8 @@ app.post('/IVS', getIVSAllocation(spawn, database));
 
 app.post('/Markowitz', getMarkowitzAllocation(spawn, database));
 
-// const pythonProcess = spawn('python',["./controllers/Strategies/IVS/sample.py", 450]);
-// pythonProcess.stdout.on('data', (data) => {
-//     console.log(data.toString());
-// });
 
-// const port  = process.env.PORT || 3000;
-const port  = 3000;
+
+const port  = process.env.PORT || 3000;
+
 app.listen( port, () => {console.log(`Listening to request on port ${port}!`)});
