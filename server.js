ee9ca25aaeeddd26856d7c4b65ec238eeb9f74c3
pranav2308 <pranav08@terpmail.umd.edu>
2020-02-14 14:40:48 -0500

    Updated connection for heroku database

--- server.js
+++ server.js
@@ -16,10 +16,8 @@ var knex = require('knex')
 const database = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'pranav2308',
-    database : 'beta_manager'
+    connectionString: process.env.DATABASE_URL,
+  	ssl: true
   }
 });
 
